3d2d405ac5c461e261022ad12949e165d58f91f6
Rodrigo <rodrigo.prestes@gmail.dom>
2020-08-06 19:28:59 -0300

    #4

--- src/test/java/orion/talk/TalkServiceIT.java
+++ src/test/java/orion/talk/TalkServiceIT.java
@@ -2,31 +2,36 @@ package orion.talk;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import javax.inject.Inject;
+import java.io.File;
+import java.time.Duration;
 
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+import org.microshed.testing.jaxrs.RESTClient;
+import org.microshed.testing.jupiter.MicroShedTest;
+import org.microshed.testing.testcontainers.ApplicationContainer;
 import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitStrategy;
+import org.testcontainers.junit.jupiter.Container;
 
 import orion.talk.secure.ProtectedService;
 
-@Testcontainers
+@MicroShedTest
 public class TalkServiceIT {
 
-    // https://www.testcontainers.org/modules/docker_compose/
-    @ClassRule
-    public static DockerComposeContainer environment = new DockerComposeContainer();
+    @Container
+    public static ApplicationContainer talk = new ApplicationContainer().withAppContextRoot("/orion-talk-service")
+            .withExposedPorts(9081).waitingFor(Wait.forHttp("/"));
 
-    @Inject
-    @RestClient
-    public static ProtectedService talk;
+    @RESTClient
+    public static ProtectedService client;
 
     @Test
     public void createChannelTest() {
         // Channel channel = talk.createChannel();
         assertTrue(true);
+
     }
 
 }
